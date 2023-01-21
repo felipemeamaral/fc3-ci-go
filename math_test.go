6b24113c3c75617e532e8fd0093eff9f00256e45
Felipe Amaral <pulga.lipe@gmail.com>
2023-01-21 12:55:18 -0300

    feat: adicionado nova feature de validacao de numeros

--- math_test.go
+++ math_test.go
@@ -3,9 +3,9 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	total := Soma(10, 10)
+	total := Soma(15, 15)
 
-	if total != 20 {
-		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado %d", total, 20)
+	if total != 30 {
+		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado %d", total, 30)
 	}
 }
